fe94db3ee496f520f245c6f356113bc1d5245901
glades18 <doradomiguel35@gmail.com>
2019-07-18 16:16:58 +0800

    completed task for basic_operator

--- basic_operators.py
+++ basic_operators.py
@@ -1,1 +1,62 @@
-a= 10
+    def subtract(x, y):
+            difference = x - y
+            print(difference)
+            return differnce
+    
+
+    def multiply(x, y):
+            print(x * y)
+    
+
+    def divide(x, y):
+            x = 3
+            y = 5
+            quotient = y / x
+            print(quotient)
+    
+
+    def add(c, d):
+            sum = c + d
+            return sum
+
+
+    def modulus(a, b):
+            return b % a
+    
+
+    def exponent(a, b):
+            b = 4
+            result = a ** b
+            return result
+
+
+    def add_subtract(s, t):
+            s = 200
+            print(s - t+5)
+    
+
+    def multiply_divide(a, b, c):
+            a = 50
+            b = 3
+            c = 45
+            print(b*c / a)
+
+
+    def add_divide(a, b, c):
+            a = 50
+            b = 3
+            c = 45
+            return b/c + a
+
+
+    def subtract_multiply(a, b, c):
+            a = 50 
+            b = 3
+            c = 45
+            return a - c*b
+
+
+            
+            
+
+    
