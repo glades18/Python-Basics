a2c2d54939333f78caf8f22ee02a4abcad7c111f
glades18 <doradomiguel35@gmail.com>
2019-07-18 19:01:10 +0800

    completed task for numbers

---
+++ numbers.py
@@ -0,0 +1,90 @@
+    def display_number(num):
+            print('Numbere to display is ' + num)
+
+    
+    def add_number(x, y, z):
+            x = 3
+            y = 10
+            z = 7
+
+            total = x + y + z
+
+            while total <= 20 and total > 0:
+                    print('Sum is below 20 and greater than 0') 
+
+            return total
+
+
+    def count_list(num_list):
+            count = 0
+
+            for index in num_list:
+                    print(count)
+                    count += 1
+
+            return count
+
+
+    def qualification(age, work_experience):
+            qualified_to_join = True
+
+            if age > 30 and work_experience > 10:
+                     return qualified_to_join
+            else:
+                    qualified_to_join= False
+                    return qualified_to_join
+
+
+    def add_list(number):
+            my_list = [0, 1, 2, 3]
+
+            my_list.append([number])
+
+            return my_list
+
+
+    def create_list(stock, repeat):
+            my_list = []
+
+            while repeat != 0:
+                    mylist.append([number])
+                    repeat -= 1
+
+
+    def determine_age_in_the_next_years(age, years):
+            total = age + years
+            print('Your age in the next ' + years + ' is ' + total)
+
+            return total
+
+
+    def combine_basic_operators(first_num, second_num, third_num, fourth_num):
+            result = first_num/third_num + second_num*fourth_num - 100
+
+            return result
+
+
+    def multiply_numbers(first_number, second_number):
+            print('We multiply ' + second_number + ' with ' + first_number)
+
+            result = second_number * first_number
+
+            print('Result is ' + 'result')
+
+            return result
+
+
+    def even_odd(num):
+            if num % 2 == 0:
+                    print('Number is Even')
+            else:
+                print('Number is Odd')
+            
+          
+
+
+
+
+    
+            
+                    
