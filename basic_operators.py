dc9421ed8760ec5733be4f6fc7c1b2cb96ebf406
glades18 <glad.ensoy@gmail.com>
2019-07-19 17:25:05 +0800

    finishing touches

--- basic_operators.py
+++ basic_operators.py
@@ -1,59 +1,69 @@
-    def subtract(x, y):
+def subtract(x, y):
             difference = x - y
             print(difference)
-            return differnce
     
+            return difference
 
-    def multiply(x, y):
-            print(x * y)
+print(subtract(x, y))
+
+
+def multiply(x, y):
+            return x * y
     
+print(multiply(x, y))
 
-    def divide(x, y):
-            x = 3
-            y = 5
+
+def divide(x, y):
             quotient = y / x
             print(quotient)
-    
 
-    def add(c, d):
+divide(x, y)
+
+    
+def add(c, d):
             sum = c + d
+            
             return sum
 
+print(add(c, d))
 
-    def modulus(a, b):
+
+def modulus(a, b):
             return b % a
     
+modulus(a, b)
 
-    def exponent(a, b):
-            b = 4
+
+def exponent(a, b):
             result = a ** b
+            
             return result
 
+print(exponent(a, b))
 
-    def add_subtract(s, t):
-            s = 200
-            print(s - t+5)
+
+def add_subtract(s, t, u):
+            print(s - t+u)
+
+add_subtract(s, t, u)
+
     
+def multiply_divide(a, b, c):
+            return b*c / a
 
-    def multiply_divide(a, b, c):
-            a = 50
-            b = 3
-            c = 45
-            print(b*c / a)
+print(multiply_divide(a, b, c))
 
 
-    def add_divide(a, b, c):
-            a = 50
-            b = 3
-            c = 45
+def add_divide(a, b, c):
             return b/c + a
 
+print(add_divide(a, b, c))
 
-    def subtract_multiply(a, b, c):
-            a = 50 
-            b = 3
-            c = 45
+
+def subtract_multiply(a, b, c):
             return a - c*b
+
+print(subtract_multiply(a, b, c))
 
 
             
